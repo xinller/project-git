c3cc30839d196e3d4293af99207f30440f0b7e14
xinller <xl1012796466@gmail.com>
2020-12-21 23:48:55 +0800

    about

---
+++ src/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="about-container">
+        about page
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'about',
+    data() {
+        return {}
+    },
+}
+</script>
+
+<style lang="less" scoped></style>
